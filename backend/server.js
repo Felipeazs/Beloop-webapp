49b3bc508ab8f28a1884a48150aea1185e411980
felipeazs <felipeazs@gmail.com>
2022-10-27 10:40:09 -0300

    host updated

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ fastify.setErrorHandler(async (error, request, reply) => {
 
 const PORT = process.env.PORT || 5000
 connectDB().then(res => {
-    fastify.listen({ port: PORT }, (err, address) => {
+    fastify.listen({ port: PORT, host: '0.0.0.0' }, (err, address) => {
         if (err) {
             fastify.log.error(err)
             process.exit(1)
