f97c63fc61ae62513af7a6d383ed10de8d391c84
felipeazs <felipeazs@gmail.com>
2022-12-10 13:36:00 -0300

    server domain updated

--- backend/server.js
+++ backend/server.js
@@ -56,7 +56,7 @@ fastify.setErrorHandler(async (error, request, reply) => {
 
 const PORT = process.env.PORT || 5000
 connectDB().then(res => {
-    fastify.listen({ port: PORT, host: '0.0.0.0' }, (err, address) => {
+    fastify.listen({ port: PORT, host: 'https://beloop-webapp-production.up.railway.app' }, (err, address) => {
         if (err) {
             fastify.log.error(err)
             process.exit(1)
